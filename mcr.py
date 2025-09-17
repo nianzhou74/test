b757293d29b64ac718d2dc257d74d0ebde70c12a
nianzhou74 <1784556065@qq.com>
2025-09-17 11:14:30 +0800

    Update mcr.py
    check if input is valid;
    change the logic of turn switching

--- mcr.py
+++ mcr.py
@@ -1,5 +1,4 @@
 def is_win(game):
-    win = False
     # Check rows
      for i in range(3):
         if game[i][0] == game[i][1] == game[i][2] != ' ':
@@ -9,21 +8,21 @@ def is_win(game):
         if game[0][j] == game[1][j] == game[2][j] != ' ':
             return True
     # Check diagonals
-    if game[0][0] == game[1][1] == game[2][2] and (game[0][0] == 'X' or game[0][0] == 'O'):
-        win = True
-    if game[0][2] == game[1][1] == game[2][0] and (game[0][2] == 'X' or game[0][2] == 'O'):
-        win = True
-    return win
+    if game[0][0] == game[1][1] == game[2][2] != ' ':
+        return True
+    if game[0][2] == game[1][1] == game[2][0] != ' ':
+        return True
+    return False
 
 def main():
     game = [[' ' for _ in range(3)] for _ in range(3)]  # Tic-tac-toe board
     player1 = 'X'
     player2 = 'O'
-    turn = False  # False for player 1's turn, True for player 2's turn. Player 1 first.
+    turn = 0  # 0 for player 1's turn, 1 for player 2's turn. Player 1 first.
     print("X = Player 1")
     print("O = Player 2")
     for n in range(9):
-        turn = not turn  # Switch turns
+        current_player = 'X' if turn == 0 else 'O'
         if not turn:
             print("Player 1: ", end="")
         else:
@@ -32,13 +31,18 @@ def main():
         i, j = map(int, input().split())
         i -= 1
         j -= 1
+        if 0 <= i < 3 and 0 <= j < 3 and game[i][j] == ' ':
+                return i, j
+            else:
+                print("Invalid move. Try again.")
         if not turn:
             game[i][j] = 'X'
         else:
             game[i][j] = 'O'
         if is_win(game):
-            print("Win!")
+            print(f"Player {turn+1} wins!")
             break  # Terminate the game
+        turn = 1 - turn    # switch turn
         if n == 8:  # All cells have been filled
             print("Tie!")
         # Show the game board 
